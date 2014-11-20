35b06bcb828aa542488967edbedad36728e0d3d7
vignesh <vigneshb.hp@gmail.com>
2014-11-19 21:32:56 -0500

    Regular edit

--- src/edu/wpi/first/wpilibj/templates/Robot3331.java
+++ src/edu/wpi/first/wpilibj/templates/Robot3331.java
@@ -20,7 +20,7 @@ import edu.wpi.first.wpilibj.templates.commands.TelopCommandGroup;
  * functions corresponding to each mode, as described in the IterativeRobot
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the manifest file in the resource
- * directory.
+ * directory. 
  */
 public class Robot3331 extends IterativeRobot {
 
