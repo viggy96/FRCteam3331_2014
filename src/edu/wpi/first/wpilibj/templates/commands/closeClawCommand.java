cb18425ad305454ff2b0873bac3847783300049b
vignesh <vigneshb.hp@gmail.com>
2014-11-21 17:08:34 -0500

    Compressor debug

--- src/edu/wpi/first/wpilibj/templates/commands/closeClawCommand.java
+++ src/edu/wpi/first/wpilibj/templates/commands/closeClawCommand.java
@@ -33,10 +33,12 @@ public class closeClawCommand extends CommandBase {
     // Called once after isFinished returns true
     protected void end() {
         PneumaticSubsystem.initState();
+        PneumaticSubsystem.compressor();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        PneumaticSubsystem.compressor();
     }
 }
